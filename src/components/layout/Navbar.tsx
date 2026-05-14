3b3eb2f31162ed934a2c499b069432fca23b80c0
Samsursekh <sekhsamsur44@gmail.com>
2026-05-14 13:45:00 +0530

    Framer motion line has changed

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { motion, AnimatePresence } from 'framer-motion';
+
 import { useState, useEffect, memo } from 'react';
-import { motion, AnimatePresence } from 'motion/react';
 import { Menu, X } from 'lucide-react';
 import { cn } from '../../lib/utils';
 import { useMagnetic } from '../../hooks/useMagnetic';
